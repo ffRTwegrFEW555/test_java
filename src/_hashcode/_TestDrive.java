2a8c35dd081390602d1f0f3a7f564aac5e9b9759
Vadim Gamaliev <gamaliev-vadim@yandex.ru>
2016-10-05 17:12:45 +0500

    update

---
+++ src/_hashcode/_TestDrive.java
@@ -0,0 +1,7 @@
+package _hashcode;
+
+/**
+ * Created by USER on 05.10.2016.
+ */
+public class _TestDrive {
+}
---
+++ src/_regex/_TestDrive.java
@@ -0,0 +1,7 @@
+package _regex;
+
+/**
+ * Created by USER on 03.10.2016.
+ */
+public class _TestDrive {
+}
