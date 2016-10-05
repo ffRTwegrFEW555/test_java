f0fcbda5f9a9ede6d9acafe8c48757c88c400214
Vadim Gamaliev <gamaliev-vadim@yandex.ru>
2016-10-05 17:15:47 +0500

    Update?

--- src/_hashcode/_TestDrive.java
+++ src/_hashcode/_TestDrive.java
@@ -4,4 +4,8 @@ package _hashcode;
  * Created by USER on 05.10.2016.
  */
 public class _TestDrive {
+    public static void main(String[] args) {
+        System.out.println(new Object().hashCode());
+        System.out.println(new Object().hashCode());
+    }
 }
--- src/_regex/_TestDrive.java
+++ src/_regex/_TestDrive.java
@@ -4,4 +4,13 @@ package _regex;
  * Created by USER on 03.10.2016.
  */
 public class _TestDrive {
+    public static void main(String[] args) {
+        String symbols = "abcd 123";
+        String digitsWithSpace = "123 ";
+        String digits = "123";
+
+        System.out.println(symbols.matches("[0-9]+"));
+        System.out.println(digitsWithSpace.matches("[0-9]+"));
+        System.out.println(digits.matches("[0-9]+"));
+    }
 }
