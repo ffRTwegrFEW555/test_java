996eb48df15fdcc7573025e9bb39cc685a8d9432
Vadim Gamaliev <gamaliev-vadim@yandex.ru>
2016-10-05 17:29:58 +0500

    Revert "update??"
    This reverts commit cc77f98c6d35d01920fb8a77b8261f3601cf5d39.

--- src/_hashcode/_TestDrive.java
+++ src/_hashcode/_TestDrive.java
@@ -7,6 +7,5 @@ public class _TestDrive {
     public static void main(String[] args) {
         System.out.println(new Object().hashCode());
         System.out.println(new Object().hashCode());
-        // commit?
     }
 }
